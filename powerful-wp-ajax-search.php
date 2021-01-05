94cc097a142743915a8165522d3e9812aec5baa7
rajkuppu <kuppuraj@live.in>
2021-01-05 17:24:45 +0530

    Start: Plugin main file commit

---
+++ powerful-wp-ajax-search.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Powerful WP Ajax Search
+ * Plugin URI: https://wordpress.org/plugins/powerful-wp-ajax-search/
+ * Description: Replacement of default WordPress search
+ * Version: 0.9.0
+ * Author: Kuppuraj
+ * Author URI: https://github.com/skuppuraj
+ */
+
+if(!defined( 'ABSPATH' )){ exit;}
+
