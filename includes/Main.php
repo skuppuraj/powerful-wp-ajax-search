35ad48634061fdede9297801be1312c0e48a21ff
rajkuppu <kuppuraj@live.in>
2021-01-05 20:39:18 +0530

    Feat: Base class file added

---
+++ includes/Main.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace PWAS;
+
+class Main{
+
+}
--- powerful-wp-ajax-search.php
+++ powerful-wp-ajax-search.php
@@ -10,3 +10,17 @@
 
 if(!defined( 'ABSPATH' )){ exit;}
 
+define( 'PWAS_VERSION', '0.9.0' );
+define( 'PWAS_MAIN_FILE', __FILE__ );
+define( 'PWAS_PATH', plugin_dir_path( PWAS_MAIN_FILE ) );
+
+use PWAS\Main;
+
+require_once PWAS_PATH . 'vendor/autoload.php';
+
+function pwas_plugin_init() {
+	global $pwas;
+	$pwas = new Main();
+}
+
+pwas_plugin_init();
