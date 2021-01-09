b96970f1725f23437f389882fcb51d49f9be0882
rajkuppu <kuppuraj@live.in>
2021-01-10 00:08:10 +0530

    Package: Post build process plugin file creation

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,27 @@
 const devPort = 8081;
+const WebpackShellPlugin = require('webpack-shell-plugin');
 process.env.VUE_APP_VERSION = require('./package.json').version;
+const productionDir = "./dist";
+const outPath = `${productionDir}/powerful-wp-ajax-search`;
+const zipName = `powerful-wp-ajax-search-${process.env.VUE_APP_VERSION}.zip`;
+let production = [];
+const isProduction = process.env.NODE_ENV === 'production';
+
+if (isProduction) {
+  production.push(
+    new WebpackShellPlugin({
+      safe: true,
+      onBuildStart:[
+        'rm -rf ' + productionDir + ' && mkdir -p ' + productionDir,
+      ],
+      onBuildEnd:[
+        "npx cpy --parents '.' '!./public' '!./dist/favicon.ico' '!./dist/index.html' !./src !./src/**/scss '!./config' '!./tests' '!./cypress' '!./**/node_modules' '!./**/__debugger1.php' '!./vue.config.js' '!./babel.config.js' '!./_dev_config.php' '!./webpack.config.js' '!./postcss.config.js' '!./package.json' '!./package-lock.json' '!./composer.json' '!./composer.lock' " + outPath
+        + " && cd " + productionDir
+        + " && zip --recurse-paths " + zipName + " ./powerful-wp-ajax-search"
+      ]
+    })
+  )
+}
 
 module.exports = {
   devServer: {
@@ -23,6 +45,7 @@ module.exports = {
     optimization: {
       splitChunks: false,
     },
+    plugins:production
   },
   filenameHashing: true,
   css: {
@@ -30,4 +53,5 @@ module.exports = {
       filename: `css/pwas-${process.env.VUE_APP_VERSION}.css`,
     },
   },
+  productionSourceMap:false
 };
