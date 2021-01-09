bc94a09e350733d8f3014e7dc966ca5baabcda90
rajkuppu <kuppuraj@live.in>
2021-01-09 16:22:11 +0530

    Improve: Search bar alignment

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="pwas-search-component">
-      <h1>Search</h1>
+      <div id="pwas-search-bar" class="horizontal layout center">
+        <div id="pwas-close-button">
+          <button class="pwas-back-button" aria-label="Close">
+            <div class="pwas-back-svg">
+              <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false">
+                    <g>
+                      <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
+                    </g>
+              </svg>
+            </div>
+          </button>
+        </div>
+        <div style="padding: 0 12px;" class="pwas-flex">
+          <input id="paw-search-text" aria-label="Enter search text here and the search results will be automatically updated as you type" autocomplete="off" spellcheck="false" placeholder="search text">
+        </div>
+      </div>
   </div>
 </template>
 
